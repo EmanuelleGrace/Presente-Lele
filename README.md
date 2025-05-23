64933736bf3ab1144ae28035d799f87190627234
EmanuelleGrace <emanuellegrace07@gmail.com>
2025-05-23 12:35:25 -0300

    pages

---
+++ README.md
@@ -0,0 +1,18 @@
+# 💖 Projeto_lele
+Bem-vindo ao Projeto lele, um projeto feito por mim, para o amor da minha vida
+
+✨ Descrição
+
+Este projeto é dedicado a demonstrar 1% do que sou capaz de fazer pela pessoa que eu amo com todo meu coração, e focado em fazer uma surpresa completamente diferente por ser o meu primeiro projeto feito completamente do zero.
+
+🌱 Objetivos
+
+Demonstrar um pouco do meu amor pra quem eu amo
+
+Fazer o amor da minha vida feliz
+
+🎯 Uso
+
+Acesse o link:
+
+❤️ Feito com muito amor, para o meu amor. 🌹
---
+++ script.js
@@ -0,0 +1,44 @@
+// Data em que o cronometro vai iniciar a contagem
+
+const data = new Date("2024-07-02T23:59:00")
+
+const textoFinal = document.getElementsByClassName("texto-final")
+
+
+// Função que vai calcular a diferença e atualizar a tela
+
+function atualizaRelogio() {
+    const agora = new Date()
+    const diferenca = data - agora
+
+    //a função Math.abs tira o sinal de negativo, permitindo que conte até a data e quando essa data passar, continue contando
+    let segundos = Math.abs(Math.floor(diferenca / 1000))  
+
+    // Quebra os seguundos em dias, horas, minutos e segundos restantes
+    const dias = Math.floor(segundos / (60 * 60 * 24))
+    const horas = Math.floor((segundos % (60 * 60 * 24)) / (60 * 60))
+    const minutos = Math.floor((segundos & (60 * 60)) / 60)
+    const segundosRestantes = segundos % 60
+
+    //Exibe o resultado no elemento que tem o id=relogio
+
+    document.getElementById("relogio").textContent = 
+     `São\n${dias.toString().padStart(2, '0')}d `+
+     `${horas.toString().padStart(2, '0')}h `+
+     `${minutos.toString().padStart(2, '0')}m `+
+     `${segundosRestantes.toString().padStart(2, '0')}s \n Sendo feliz ao seu lado.`;
+
+}
+
+
+// Espera todo o html carregar para iniciar a contagem e atualizar o relogio a cada segundo
+
+document.addEventListener("DOMContentLoaded", function() { 
+    atualizaRelogio()
+    setInterval(atualizaRelogio, 1000)
+})
+
+
+
+
+
